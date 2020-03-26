71ba03950034dc01c6a2f55b7bbfb1287bbf0fdd
guyshoham <guyshoham28@gmail.com>
2020-03-26 13:50:06 +0200

    documentation

--- ex11.sh
+++ ex11.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+# Name: Guy Shoham, ID: 302288444
+
 declare -i count=0
 cat file.txt | while read line
 do
+	#calculate the number of words of the line (if it contains 'text'), and write to a temp flle
 	echo $line | grep text | wc -w > tmp
+	#save the temp file content to a var
 	read var < tmp
 	count=$((count+1))
+	#if words count is not 0, so the word 'text' is shown in that line
 	if test $var -ne "0"
 	then
 		echo -ne $[count]" " 
--- ex12.sh
+++ ex12.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
+# Name: Guy Shoham, ID: 302288444
 
 cd $1
+# for each file or directory in list
 for i in $(ls)
 	do	
+		# check if the extension is .txt
 		if [[ ${i: -4} == ".txt" ]]; then
 			echo "$i is a file" >> tmp.txt
 		fi
 	done
+# print content of fils sorted
 cat tmp.txt | sort
 rm tmp.txt
 
 for j in $(ls)
 	do
+	# check if j is a directory
 		if [ -d $j ]; then
 			echo "$j is a directory" >> tmp.txt	
 		fi
