819165613b1780d689c710607682cd964e5c0130
guyshoham <guyshoham28@gmail.com>
2020-03-26 12:05:06 +0200

    ex11.sh + file.txt

---
+++ ex11.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+declare -i count=0
+cat file.txt | while read line
+do
+	echo $line | grep text | wc -w > tmp
+	read var < tmp
+	count=$((count+1))
+	if test $var -ne "0"
+	then
+		echo -ne $[count]" " 
+		echo $line | wc -w		
+	fi
+done
