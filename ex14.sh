f8a9796458a42d39a8a7245fde00c9f804a3bcad
guyshoham <guyshoham28@gmail.com>
2020-03-26 18:33:44 +0200

    documentation

--- ex14.sh
+++ ex14.sh
@@ -3,18 +3,19 @@
 
 sum=0
 
+# read file line by line
 cat $2 | while read line
 do
 	echo $line | grep "$1" > tmp
-	read var < tmp
+	read var < tmp # var will contain a string only if line contains the correct name
 	if [ -n "$var" ]
 	then
 		echo $line
 		arr=($var)
-		echo ${arr[2]} > tmp
-		read value < tmp
-		sum=$((sum+value))
-		echo $sum > balance
+		echo ${arr[2]} > tmp # save the integer value in tmp file
+		read value < tmp # read value from tmp file
+		sum=$((sum+value)) # add to sum
+		echo $sum > balance # save sum into another temp file (balance)
 	fi
 done
 
