aa435e298f2e6153af4049c4435e5bd1e9f78e4f
guyshoham <guyshoham28@gmail.com>
2020-04-02 14:10:09 +0300

    Update ex12.sh

--- ex12.sh
+++ ex12.sh
@@ -1,25 +1,27 @@
 #!/bin/bash
 # Name: Guy Shoham, ID: 302288444
 
+declare -i count=0
+
 cd $1
 # for each file or directory in list
-for i in $(ls)
+for i in $(ls -v)
 	do	
 		# check if the extension is .txt
 		if [[ ${i: -4} == ".txt" ]]; then
-			echo "$i is a file" >> tmp.txt
+			count=$((count+1))
+			echo -ne $[count]" " 
+			echo "$i is a file"
 		fi
 	done
 # print content of fils sorted
-cat tmp.txt | sort
-rm tmp.txt
 
 for j in $(ls)
 	do
 	# check if j is a directory
 		if [ -d $j ]; then
-			echo "$j is a directory" >> tmp.txt	
+			count=$((count+1))
+			echo -ne $[count]" " 
+			echo "$j is a directory"
 		fi
 	done
-cat tmp.txt | sort
-rm tmp.txt
