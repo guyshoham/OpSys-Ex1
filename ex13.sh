255d8515927a80618b537f74acbbdf1865482ef8
guyshoham <guyshoham28@gmail.com>
2020-04-03 15:04:34 +0300

    Update ex13.sh

--- ex13.sh
+++ ex13.sh
@@ -9,9 +9,11 @@ for i in $(ls -v)
 			cd $i
 			if test -f "$2"; then
 				cat $2 #print file content
+				echo
 			fi
 			cd ..
 		elif [ $i == $2 ]; then # check if current file name is equal to target file name
 			cat $2 # print file content
+			echo
 		fi
 	done
